8f35291f970b04b055f6223d5893116d48374631
Corey Donenfeld <corey@studiosimpati.co>
2023-02-24 09:59:11 -0500

    Adding test cases and using JSON.stringify to do a quick check to compare the outputs of the return values of the native and custom property of the testcase.

--- tests.js
+++ tests.js
@@ -1,5 +1,22 @@
 require('./javascript-methods.js');
 
-let testArr = [1, 2, 3, 4, 5];
-
-console.log(testArr.myMap((x) => x * 2));
+// 
+let testArray = [1, 2, 3, 4, 5];
+let testCases = [
+    {
+        label: 'myMap: x + 1',
+        native: testArray.map((x) => x + 1),
+        custom: testArray.myMap((x) => x + 1),
+    },
+    {
+        label: 'myMap: x * 2',
+        native: testArray.map((x) => x * 2),
+        custom: testArray.myMap((x) => x * 2),
+    },
+]
+testCases.forEach((testCase) => {
+    console.log(testCase.label);
+    console.log("Native:", testCase.native);
+    console.log("Custom:", testCase.custom);
+    console.log("Pass:", JSON.stringify(testCase.native) === JSON.stringify(testCase.custom));
+});
